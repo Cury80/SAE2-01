b6af652e91d89ada62ddcd2824adeee31c74546f
Léo Seguin <estebaleo@hotmail.fr>
2022-05-20 09:25:34 +0200

    Ajoutez des fichiers projet.

---
+++ Bibliothèque accès serveur/Class1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using MySql.Data;
+
+namespace Bibliothèque_accès_serveur
+{
+    public class BDD
+    {
+        private static MySqlConnection maCnx;
+
+        public static bool initConnexion()
+        {
+            string serveur = "10.1.139.235";
+            string login = "f5";
+            string passwd = "mdp";
+            string BD = "basef5";
+            string chaineDeConnexion = $"server={serveur};uid={login};pwd={passwd};database={BD}";
+
+            maCnx = new MySqlConnection(chaineDeConnexion);
+            try
+            {
+                maCnx.Open();
+                Console.WriteLine("ça passe mon gars");
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                return false;
+            }
+        }
+
+        public static bool FermerConnexion()
+        {
+            try
+            {
+                if (maCnx.State == System.Data.ConnectionState.Open)
+                {
+                    maCnx.Close();
+                    maCnx.Dispose();
+                }
+                return true;
+            }
+            catch (Exception ex1)
+            {
+                Console.WriteLine(ex1.ToString());
+                return false;
+            }
+        }
+    }
+}
---
+++ Page d'accueil/PageDaccueil.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Page_d_accueil
+{
+    public partial class PageDaccueil : Form
+    {
+        public PageDaccueil()
+        {
+            InitializeComponent();
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PageDaccueil_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
