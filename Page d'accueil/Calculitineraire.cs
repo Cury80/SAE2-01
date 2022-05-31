b0201360b66896a6d7905bcccc6afa7341196431
Lucas Priez <priez.lucas80@gmail.com>
2022-05-31 22:36:34 +0200

    Initialisation de la class Calculateur d'itinéraire

--- Page d'accueil/Calculitineraire.cs
+++ Page d'accueil/Calculitineraire.cs
@@ -12,9 +12,33 @@ namespace Calculateur_d_itinéraire
 {
     public partial class Calculitineraire : Form
     {
+        ///<summary>
+        ///Déclaration des différentes variables de la classe Calculateur_itineraire
+        ///</summary>
+
+        class Calculateur_itineraire
+        {
+            protected:
+                int distance = 0;
+                int distance_temp = 0;
+                int[] PointParcouru = new int[31];
+                int remplissage_point_parcouru = 2;
+                int temp_remplissage_Point_Parcouru = 2;
+                int[] successeur_depart = new int[31];
+                int[] successeur_depart_temp = new int[31];
+                int[] itineraire = new int[31];
+                int[] itineraire_temp = new int[31];
+                
+            public:
+                int Calculateur_itineraire(int, int, int[][]);         
+        }
+    }
+
         public Calculitineraire()
         {
             InitializeComponent();
         }
+
+
     }
 }
