b2f3af36c102ca93b708730fbf43335dff8270b8
Lucas Priez <priez.lucas80@gmail.com>
2022-06-03 00:02:20 +0200

    Revert "modification sur calculitineraire"
    This reverts commit 550424d33c996c70e43b3b8028edc2aa38abfa79.

--- Page d'accueil/Calculitineraire.cs
+++ Page d'accueil/Calculitineraire.cs
@@ -11,22 +11,14 @@ using System.Windows.Forms;
 namespace Calculateur_d_itinéraire
 {
     public partial class Calculitineraire : Form
-    { 
-        public Calculitineraire()
-        {
-            InitializeComponent();
-        }
-
-        // Objet dans lequel on va faire la matrice, Requete à la base , on rempli la matrice des poids matrice carrè m[31][31], La matrice des distances dans le cadre de dijkstra, et la matrice des prédecesseurs
-        //class calculateur itineraire, objet matrice des poids, objet matrice des distances, objet matrice des prédecesseurs
-        
+    {
         ///<summary>
         ///Déclaration des différentes variables de la classe Calculateur_itineraire
         ///</summary>
 
         class Calculateur_itineraire
         {
-            private int distance;   //nombre de minutes necessaire pour aller du départ à l'arrivée
+            private int distance;   //nombre de minutes nessecaire pour aller du départ à l'arrivée
             private int distance_temp;  // distance temporaire qu'on ajoute si c'est la distance la plus courte 
             private int[] PointParcouru = new int[31];  //regroupe les points qui ont été compté par le calculateur
             private int remplissage_point_parcouru = 2; //indice de remplissage du tableau point parcouru
@@ -37,16 +29,21 @@ namespace Calculateur_d_itinéraire
             private int[] itineraire_temp = new int[31];    //liste temporaire jusqu'a ce qu'elle soit validée et copié dans itineraire
             private int point_pivot;    //  point suivant le point précédement visité  
 
-            public Calculateur_itineraire(int, int, int[][]) //constructeur du calculateur
-            {
-
-            }   
+            public Calculateur_itineraire(int, int, int[][]);   //constructeur du calculateur
             public void init_Calculateur(); //initialisateur du constructeur
             public void Calculateur();  //le sous programme du calculateur
             public ~Calculateur_itineraire();
         }
 
         
+        
+
+        public Calculitineraire()
+        {
+            InitializeComponent();
+        }
+
+
         ///<summary>
         ///Initialisateur du Calculateur qui prend en paramètres la station de départ, la station d'arrivé et la matrice de graph du réseau
         ///<\summary>
@@ -81,70 +78,6 @@ namespace Calculateur_d_itinéraire
 
         }
 
-        private void Calculitineraire_Load(object sender, EventArgs e)
-        {
 
-        }
     }
 }
-
-/*
-Objet Matrice_De_Toute_Distance
-protected :
-int[31][31] // vecteur
-
-constructeur Matrice_De_Toute_Distance(a){
-
-}
-
-~Matrice_De_Toute_Distance(){}
-
-Objet tableau_Distance : Matrice_DeTouteDistance
-protected:
-int[31][31] // à transformer en vecteur
-constructeur tableau_Distance(int){
-
-}
-miseAJour(int){
-
-}
-~tableau_Distance(){}
-
-Objet tableau_Prédecesseur : Matrice_DeTouteDistance
-protected :
-int[31][31] // vecteur
-
-constructeur tableau_Prédecesseur(a){
-
-}
-miseAJour(int){
-
-}
-~tableau_Prédecesseur(){}
-
-Objet Calculateur : tableau_Prédecesseur, tableau_Distance
-protected :
-int[31] pointAParcourir // vecteur les deux
-int[31] pointParcouru
-int temps
-
-constructeur calculateur(int a) : tableau_Prédecesseur(a) : tableau_Distance(a){
-
-
-}
-~calculateur(){}
-calcul(int,int){
-
-}
-Affichage(){
-
-}
-
-main{
-int a,b;
-calculateur();
-Veuillez entrer la station de départ : get;
-Veuillez entrer la station d'arrivée : get;
-calcul(a,b);
-}
-*/
